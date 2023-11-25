0c8d0011030a1c3fafdca6ab1568a3a48421c8a7
gvsvvs <123079849+gvsvvs@users.noreply.github.com>
2023-11-25 21:20:04 +0530

    Add files via upload

--- FULL_PROJECT.asm
+++ FULL_PROJECT.asm
@@ -1,8 +1,3 @@
-; This file detects the pressed key on a 4x4 keypad
-; and displays the character pressed on the seven segment display.
-
-
-
 MYDATA   SEGMENT 
     
  
@@ -14,24 +9,26 @@ MYDATA   SEGMENT
          
          SSPA EQU 0000H
          SSPB EQU 0002H
-         SSPC EQU 0004H
+         SSPC EQU 0004H    ;port addresses for SSDs interfacing
          SSCR EQU 0006H 
+         
          KBPA EQU 0020H
-         KBPB EQU 0022H
+         KBPB EQU 0022H    ;port addresses for Keypad  interfacing
          KBPC EQU 0024H
          KBCR EQU 0026H
          
          MSC EQU 3003H
-         MSB EQU 3002H
+         MSB EQU 3002H     ;temporary usage for memory
          MSA EQU 3001H 
           
          PORTD EQU 00E0H 
-         PORTE EQU 00E2H
+         PORTE EQU 00E2H   ;port addresses for temperature sensor interfacing
          PORTF EQU 00E4H
-         CONTROLB EQU 00E6H 
+         CONTROLB EQU 00E6H  
+         
          PORTG EQU 0060H 
          PORTH EQU 0062H
-         PORTI EQU 0064H
+         PORTI EQU 0064H      ;port addresses for buzzer interfacing
          CONTROLC EQU 0066H
          ;Bit pattern for Dispaying Numerals on SSD
          SSD_TABLE DB 3FH, 06H, 5BH, 4FH, 66H, 6DH, 7DH, 07H, 7FH, 6FH
@@ -69,20 +66,20 @@ MYCODE   SEGMENT
          ; PORTA as OUTPUT, PORTB as OUTPUT and PORTC as INPUT           
     
 START:   MOV AL, 90H 
-         MOV DX, CONTROLB
+         MOV DX, CONTROLB    ;setting the port a to input(temperature sensor)
          OUT DX, AL
          
          MOV AL, 80H 
-         MOV DX, CONTROLC
+         MOV DX, CONTROLC       ;  setting all ports to output(buzzer and led lights)
          OUT DX, AL 
          
-         MOV AL, 90H 
+         MOV AL, 90H        ;seting port a to input for keypad interfacing
          MOV DX, KBCR
          OUT DX, AL   
                     
                     
          MOV AL, 80H 
-         MOV DX, SSCR
+         MOV DX, SSCR       ;  setting all ports to output for SSDs
          OUT DX, AL
          ; Initialize SSD by displayong 0
          
@@ -90,7 +87,7 @@ START:   MOV AL, 90H
          MOV BX, MSC
          MOV [BX], 3FH
          MOV BX, MSB                                 
-         MOV [BX], 3FH
+         MOV [BX], 3FH          ;initializing all SSDs to 0
          MOV BX, MSA
          MOV [BX], 3FH
          MOV DX, SSPC
@@ -109,10 +106,7 @@ AGAIN:   CALL KEY_WRITE
          CALL CONVERT_TO_BINAY
          
          MOV BX,MSA
-         MOV [BX],AL 
-         ;MOV AL,[BX]
-         ;MOV DX,SSPC
-         ;OUT DX,AL
+         MOV [BX],AL      ;taking the hundreds digit in and storing in 4000h
          MOV [4000H],AL         
          
          CALL KEY_WRITE     
@@ -121,10 +115,7 @@ AGAIN:   CALL KEY_WRITE
          CALL CONVERT_TO_BINAY
          MOV BX,MSB
          MOV [BX],AL
-         ;MOV AL,[BX]
-         ;MOV DX,SSPB
-         ;OUT DX,AL
-         MOV BL,10 
+         MOV BL,10             ;taking the tens digit in and storing in 4000h by multiplying the number in 4000h by 10 and adding this tens digit
          MOV CL,AL
          MOV AL,[4000H]
          MOV AH,00H
@@ -138,40 +129,30 @@ AGAIN:   CALL KEY_WRITE
          CALL CONVERT_TO_BINAY
          MOV BX,MSC
          MOV [BX],AL
-         ;MOV AL,[BX]
-         ;MOV DX,SSPA
-         ;OUT DX,AL
          MOV BL,10 
          MOV CL,AL
-         MOV AL,[4000H]
+         MOV AL,[4000H]             ;taking the ones digit in and storing in 4000h by multiplying the number in 4000h by 10 and adding this ones digit
          MOV AH,00H
          MUL BL
          ADD AL,CL
          MOV [4000H],AL 
-
-         ;CALL DELAY_S
-         ;CALL DELAY_S
-         ;CALL DELAY_S
-         ;CALL DELAY_S
-         ;CALL DELAY_S
-         
-         
-         
-                 
+   
          RET 
          
          FIRST:CALL AGAIN
-         MOV [1001H],AL
-         MOV DX,SSPC
+         MOV [1000H],AL
+         MOV DX,SSPC           ;storing the first threshold in 1000h
          OUT DX,AL
          SECOND:CALL AGAIN
          MOV [1002H],AL
-         MOV DX,SSPB
+         MOV DX,SSPB                    ;storing the second threshold in 1002h
          OUT DX,AL 
          
          CALL TEMPSENS 
          
-         JMP FIRST
+         JMP FIRST     
+         
+         
 ; PROCEDURE: KEY_WRITE - Write the key pressed on the SSD
 ; INPUT : NONE
 ; OUTPUT: NONE
@@ -270,7 +251,7 @@ NEXT1:   MOV DX, KBPB
          CMP AL, 0FH
          JNE ROW_2
          
-         ; Make Row0 only 0 rest of the rows 1
+         ; Make Row3 only 0 rest of the rows 1
          
          MOV DX, KBPB 
          MOV AL, 0F7H
@@ -303,35 +284,12 @@ FIND_COL: RCR AL,1
           ;MOV CX,BX
           JMP FIND_COL
           
-MATCH:   ;MOVING SSG DISPLAY
+MATCH:   ;MOVING SSD DISPLAY
 
-         
-         MOV CX,BX; 
-         
-                                
-         ;MOV BX,MSB
-         ;MOV AL,[BX]
-         ;MOV DX,SSPA
-         ;OUT DX,AL
-         ;MOV BX,MSA
-         ;MOV [BX],AL 
-         
-         ;MOV BX,MSC
-         ;MOV AL,[BX]
-         ;MOV DX,SSPB
-         ;OUT DX,AL
-         ;MOV BX,MSB
-         ;MOV [BX],AL
-                                                                
-         ;DECODING KEY AND DISPLAING IN SS C
-         ;MOV BX, CX
+         MOV CX,BX
          MOV AL, [BX]
-         MOV DX, KBPC
-         OUT DX, AL
-         ;MOV BX,MSC
-         ;MOV [BX],AL
-         
-          
+         MOV DX, KBPC  ;displaying the pressed key on SSD which is interfaced with the one the keypad is also interfaced
+         OUT DX, AL          
          
          RET  
          
@@ -341,6 +299,7 @@ KEY_WRITE ENDP
 ; PROCEDURE: DELAY_MS : Generates approx 20 ms delay
 ; INPUT : NONE
 ; OUTPUT: NONE  
+
 
 DELAY_MS PROC  NEAR  
          PUSH CX  
@@ -367,9 +326,9 @@ CONVERT_TO_BINAY PROC NEAR
         
          
         MOV CL,00H
-        CMP AL,3FH
+        CMP AL,3FH           ;comparing the offsets to map to digits 
         
-        JE STORE_B
+        JE STORE_B           ;if matched jumping to storing binary(digit)
         
         MOV CL,01H
         CMP AL,06H
@@ -410,7 +369,7 @@ CONVERT_TO_BINAY PROC NEAR
         
         
 STORE_B:NOP
-        MOV AL,CL
+        MOV AL,CL       ;instead of LED's display mumber storing the original value in AL 
         
         RET    
 CONVERT_TO_BINAY ENDP 
@@ -421,20 +380,20 @@ CONVERT_TO_BINAY ENDP
                
 TEMPSENS:      
          MOV DX, PORTD
-         IN AL, DX
+         IN AL, DX            ;reading the input from temperature sensor
          
          MOV BH,00
          MOV BL,AL  
          
          MOV CX,500
          XOR AH,AH
-         MUL CX
+         MUL CX                   ;multiplying with 500 and divide with 256 to get original tempe
          MOV CX,256
          DIV CX
          MOV BL,AL 
          CALL DELAY_S  
          CALL DELAY_S
-         MOV [2000H], BX
+         MOV [2000H], BX          ;store the read temperature in 2000h
          MOV AX,BX
          MOV CL,10
          DIV CL
@@ -443,40 +402,33 @@ TEMPSENS:
          MOV AH,00H
          
          
-         MOV AL, 80H 
+         MOV AL, 80H              ;set the SSDs interfaced 8255's ports to output 
          MOV DX, SSCR
          OUT DX, AL   
-         
-         ; Initialize SSD by displayong 0
-         
-        ; MOV AL, 3FH
-         ;MOV DX, PORTA
-         ;OUT DX, AL 
          
          
          ; Get pointer to the SSD Table
          
-         MOV SI, OFFSET SSD_TABLE 
+         MOV SI, OFFSET SSD_TABLE          ;loading the offset of the SSD_TABLE
+
          
-        ; MOV BL, 0AH    ; A counter for 10 digits
          
-         
-AGAIN1:  ADD SI, BX 
+AGAIN1:  ADD SI, BX                                ;adding to the base address by the key pressed value to display in SSD
         MOV AL, [SI]   ; get the byte from table
          
          MOV DX, SSPC  ; display on SSD
-         OUT DX, AL
+         OUT DX, AL                         ;display ones digit in LAST SSD
           
          MOV BX,[2000H]
          MOV AX,BX
          MOV CL,10
          DIV CL 
-         MOV AH,00H
+         MOV AH,00H                            ;calculate the tens digit by dividing the number in 2000h by 10
          DIV CL
          MOV BL,AH 
          MOV BH,00H
          MOV AH,00H
-         
+                                                             ;increase the base address of SSD by the remainder
          MOV SI, OFFSET SSD_TABLE 
          
         ; MOV BL, 0AH    ; A counter for 10 digits
@@ -485,7 +437,7 @@ AGAIN1:  ADD SI, BX
         ADD SI, BX 
         MOV AL, [SI] 
          
-         MOV DX, SSPB  ; display on SSD
+         MOV DX, SSPB  ; display tens in second SSD
          OUT DX, AL 
          
          MOV BX,[2000H]
@@ -495,69 +447,57 @@ AGAIN1:  ADD SI, BX
          MOV AH,00H
          DIV CL
          MOV AH,00H
-         DIV CL
+         DIV CL                             ;calculate hundreds by dividing the number in 2000h by 100 and store the remainder 
          MOV BL,AH 
          MOV BH,00H
          MOV AH,00H
          
-         MOV SI, OFFSET SSD_TABLE 
-         
-        ; MOV BL, 0AH    ; A counter for 10 digits
+         MOV SI, OFFSET SSD_TABLE           ; increase the base address by the remainder to map to the correct LED display
          
          
         ADD SI, BX 
         MOV AL, [SI] 
          
-        MOV DX, SSPA  ; display on SSD
+        MOV DX, SSPA  ; display the hundreds number in the top SSD
         OUT DX, AL
         
         
-        MOV DX,KBPC
-        MOV AL,[1002H]
-        OUT DX,AL 
         
-        MOV AX,[2000H]
-        MOV BX,50        
-        CMP AX,BX
-        JNC CAG
-        ;JMP START
+        MOV AX,[2000H]   ;load the temperature which is read in AX
+        MOV BX,[1000H]   ;load the first threshold in BX
+        XOR BH,BH      
+        CMP AX,BX        ;comapre AX and BX
+        JNC CAG          ;if the temperature read is greater than the first threshold then jump to subroutine CAG
         
-        MOV DX,KBPC
-        MOV AL,01H
+        MOV AL,00H
+        MOV DX,PORTI     ;if it is not greater then no buzzer and no light
         OUT DX,AL
-        JMP R  
+
+        JMP R            ;jump to R
         
         CAG:   
-        MOV DX,KBPC
-        MOV AL,02H
-        OUT DX,AL 
-        
         
         MOV AX,[2000H] 
-        MOV BX,100
+        MOV BX,[1002H]
+        XOR BH,BH          ;load the second threshold into bx 
         CMP AX,BX
-        JC CONTI
+        
+        JC CONTI           ;if the temperaure read is greater than the second threshold the go for high frequency Buzzer
+        
         MOV AL,0AH
-        MOV DX,PORTI
-        OUT DX,AL
-        NOP  
+        MOV DX,PORTI       ;if greater than the first threshold and less than second threshold 
+        OUT DX,AL          ;low frequency buzzer and yellow light
         CALL  DELAY_S
         JMP R
         
         CONTI: 
-        MOV DX,KBPC
-        MOV AL,07H
-        OUT DX,AL 
-        
-        
         MOV AL,05H
-        MOV DX,PORTI
+        MOV DX,PORTI       ;if temp read is greater than both the thresholds the high frequency and red light
         OUT DX,AL
          
+
         
-        
-        R:
-        JMP TEMPSENS 
+        R:JMP TEMPSENS    ;jump to tempsens until stopped read the modified temperature(if any) and act accordingly
          
          
 
@@ -580,6 +520,6 @@ DELAY_S ENDP
 
 MYCODE   ENDS
         
-         END  START   
+         END  START 
          
-;----------------- END OF FILE -------------------------------------------;
+ ;END  
